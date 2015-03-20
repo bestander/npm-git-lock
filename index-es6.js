5f5d41f5466eb183874dc75c839528f0a099b65e
Konstantin Raev <bestander@gmail.com>
2015-03-20 17:10:02 +1300

    removed --folder option because it is implied to be cwd and instead of reading version from package.json I read sha1 and use it as indicator of a need to update packages

--- index-es6.js
+++ index-es6.js
@@ -7,18 +7,21 @@ var del = require('del');
 let fs = require('fs');
 let promisify = require("es6-promisify");
 let log = require('loglevel');
+let crypto = require('crypto');
+
 let readFilePromise = promisify(fs.readFile);
 let npmiPromise = promisify(npmi);
 let delPromise = promisify(del);
 let statPromise = promisify(fs.stat);
 
 let argv = require('optimist')
-.usage('Usage: $0 --repo [git@bitbucket.org:your/git/repository.git] --folder [relative/path/to/folder/with/node_modules] --verbose')
-.describe('verbose', 'Print progress log messages')
-.demand(['repo', 'folder']).argv;
+.usage('Usage: $0 --repo [git@bitbucket.org:your/git/repository.git] --verbose')
+.describe('verbose', '[-v] Print progress log messages')
+.describe('repo', 'git url to repository with node_modules content')
+.alias('v', 'verbose')
+.demand(['repo']).argv;
 
-
-let packageJson;
+let packageJsonSha1;
 let cwd = process.cwd();
 if (argv.verbose) {
     log.setLevel("debug");
@@ -26,19 +29,19 @@ if (argv.verbose) {
     log.setLevel("info");
 }
 let repo = argv.repo;
-let folder = argv.folder;
 
-readFilePromise(`${folder}/package.json`, "utf-8")
+
+readFilePromise(`${cwd}/package.json1`, "utf-8")
 .then(function (packageJsonContent) {
-    packageJson = JSON.parse(packageJsonContent);
-    log.debug(`Read package.json version ${packageJson.version}`);
-    return packageJson;
+    packageJsonSha1 = crypto.createHash('sha1').update(packageJsonContent).digest('hex');
+    log.debug(`Sha1 of package.json is ${packageJsonSha1}`);
+    return packageJsonSha1;
 })
 .then(function () {
-    return statPromise(`${cwd}/${folder}/node_modules`)
+    return statPromise(`${cwd}/node_modules`)
     .then(function () {
         log.debug(`Checking if remote ${repo} exists`);
-        process.chdir(`${cwd}/${folder}/node_modules`);
+        process.chdir(`${cwd}/node_modules`);
         return git('git remote -v')
         .then(function (remoteCommandOutput) {
             if (remoteCommandOutput.indexOf(repo) !== -1) {
@@ -51,20 +54,20 @@ readFilePromise(`${folder}/package.json`, "utf-8")
     }, cloneRepo)
 })
 .then(function () {
-    log.debug(`${repo} is in node_modules folder, checking out ${packageJson.version} tag`);
-    process.chdir(`${cwd}/${folder}/node_modules`);
-    return git(`reset --hard ${packageJson.version}`)
+    log.debug(`${repo} is in node_modules cwd, checking out ${packageJsonSha1} tag`);
+    process.chdir(`${cwd}/node_modules`);
+    return git(`reset --hard ${packageJsonSha1}`)
     .then(null, installPackagesTagAndPustToRemote);
 })
 .then(function () {
     process.chdir(`${cwd}`);
-    log.info(`Node_modules are in sync with ${repo} ${packageJson.version}`);
+    log.info(`Node_modules are in sync with ${repo} ${packageJsonSha1}`);
     process.exit(0);
 })
 .catch(function (error) {
     try {
         process.chdir(`${cwd}`);
-        log.debug(`Failed to synchronise node_modules with ${repo} ${packageJson.version}: ${error}`);
+        log.debug(`Failed to synchronise node_modules with ${repo}: ${error}`);
         process.exit(1);
     } catch (e) {
         console.error(e);
@@ -72,9 +75,9 @@ readFilePromise(`${folder}/package.json`, "utf-8")
 });
 
 function cloneRepo() {
-    log.debug(`Remote ${repo} is not present in ${cwd}/${folder}/node_modules/.git repo`);
-    log.debug(`Removing ${cwd}/${folder}/node_modules folder`);
-    process.chdir(`${cwd}/${folder}`);
+    log.debug(`Remote ${repo} is not present in ${cwd}/node_modules/.git repo`);
+    log.debug(`Removing ${cwd}/node_modules cwd`);
+    process.chdir(`${cwd}`);
     return delPromise([`node_modules/`])
     .then(function () {
         log.debug(`cloning ${repo}`);
@@ -89,12 +92,12 @@ function installPackagesTagAndPustToRemote() {
         let options = {
             forceInstall: false
         };
-        process.chdir(`${cwd}/${folder}`);
+        process.chdir(`${cwd}`);
         return npmiPromise(options);
     })
     .then(function () {
         log.debug("All packages installed");
-        process.chdir(`${cwd}/${folder}/node_modules`);
+        process.chdir(`${cwd}/node_modules`);
         return git(`add .`);
     })
     .then(function () {
@@ -102,10 +105,10 @@ function installPackagesTagAndPustToRemote() {
     })
     .then(function () {
         log.debug("Committed, adding tag");
-        return git(`tag ${packageJson.version}`);
+        return git(`tag ${packageJsonSha1}`);
     })
     .then(function () {
-        log.debug(`Pushing tag ${packageJson.version} to ${repo}`);
+        log.debug(`Pushing tag ${packageJsonSha1} to ${repo}`);
         return git(`push ${repo} master --tags`);
     })
 }
--- index.js
+++ index.js
@@ -8,14 +8,16 @@ var del = require("del");
 var fs = require("fs");
 var promisify = require("es6-promisify");
 var log = require("loglevel");
+var crypto = require("crypto");
+
 var readFilePromise = promisify(fs.readFile);
 var npmiPromise = promisify(npmi);
 var delPromise = promisify(del);
 var statPromise = promisify(fs.stat);
 
-var argv = require("optimist").usage("Usage: $0 --repo [git@bitbucket.org:your/git/repository.git] --folder [relative/path/to/folder/with/node_modules] --verbose").describe("verbose", "Print progress log messages").demand(["repo", "folder"]).argv;
+var argv = require("optimist").usage("Usage: $0 --repo [git@bitbucket.org:your/git/repository.git] --verbose").describe("verbose", "[-v] Print progress log messages").describe("repo", "git url to repository with node_modules content").alias("v", "verbose").demand(["repo"]).argv;
 
-var packageJson = undefined;
+var packageJsonSha1 = undefined;
 var cwd = process.cwd();
 if (argv.verbose) {
     log.setLevel("debug");
@@ -23,16 +25,15 @@ if (argv.verbose) {
     log.setLevel("info");
 }
 var repo = argv.repo;
-var folder = argv.folder;
 
-readFilePromise("" + folder + "/package.json", "utf-8").then(function (packageJsonContent) {
-    packageJson = JSON.parse(packageJsonContent);
-    log.debug("Read package.json version " + packageJson.version);
-    return packageJson;
+readFilePromise("" + cwd + "/package.json1", "utf-8").then(function (packageJsonContent) {
+    packageJsonSha1 = crypto.createHash("sha1").update(packageJsonContent).digest("hex");
+    log.debug("Sha1 of package.json is " + packageJsonSha1);
+    return packageJsonSha1;
 }).then(function () {
-    return statPromise("" + cwd + "/" + folder + "/node_modules").then(function () {
+    return statPromise("" + cwd + "/node_modules").then(function () {
         log.debug("Checking if remote " + repo + " exists");
-        process.chdir("" + cwd + "/" + folder + "/node_modules");
+        process.chdir("" + cwd + "/node_modules");
         return git("git remote -v").then(function (remoteCommandOutput) {
             if (remoteCommandOutput.indexOf(repo) !== -1) {
                 // repo is in remotes, let's pull the required version
@@ -43,17 +44,17 @@ readFilePromise("" + folder + "/package.json", "utf-8").then(function (packageJs
         }, cloneRepo);
     }, cloneRepo);
 }).then(function () {
-    log.debug("" + repo + " is in node_modules folder, checking out " + packageJson.version + " tag");
-    process.chdir("" + cwd + "/" + folder + "/node_modules");
-    return git("reset --hard " + packageJson.version).then(null, installPackagesTagAndPustToRemote);
+    log.debug("" + repo + " is in node_modules cwd, checking out " + packageJsonSha1 + " tag");
+    process.chdir("" + cwd + "/node_modules");
+    return git("reset --hard " + packageJsonSha1).then(null, installPackagesTagAndPustToRemote);
 }).then(function () {
     process.chdir("" + cwd);
-    log.info("Node_modules are in sync with " + repo + " " + packageJson.version);
+    log.info("Node_modules are in sync with " + repo + " " + packageJsonSha1);
     process.exit(0);
 })["catch"](function (error) {
     try {
         process.chdir("" + cwd);
-        log.debug("Failed to synchronise node_modules with " + repo + " " + packageJson.version + ": " + error);
+        log.debug("Failed to synchronise node_modules with " + repo + ": " + error);
         process.exit(1);
     } catch (e) {
         console.error(e);
@@ -61,9 +62,9 @@ readFilePromise("" + folder + "/package.json", "utf-8").then(function (packageJs
 });
 
 function cloneRepo() {
-    log.debug("Remote " + repo + " is not present in " + cwd + "/" + folder + "/node_modules/.git repo");
-    log.debug("Removing " + cwd + "/" + folder + "/node_modules folder");
-    process.chdir("" + cwd + "/" + folder);
+    log.debug("Remote " + repo + " is not present in " + cwd + "/node_modules/.git repo");
+    log.debug("Removing " + cwd + "/node_modules cwd");
+    process.chdir("" + cwd);
     return delPromise(["node_modules/"]).then(function () {
         log.debug("cloning " + repo);
         return git("clone " + repo + " node_modules");
@@ -76,19 +77,19 @@ function installPackagesTagAndPustToRemote() {
         var options = {
             forceInstall: false
         };
-        process.chdir("" + cwd + "/" + folder);
+        process.chdir("" + cwd);
         return npmiPromise(options);
     }).then(function () {
         log.debug("All packages installed");
-        process.chdir("" + cwd + "/" + folder + "/node_modules");
+        process.chdir("" + cwd + "/node_modules");
         return git("add .");
     }).then(function () {
         return git("commit -a -m \"updated package.json, freezing changes\"");
     }).then(function () {
         log.debug("Committed, adding tag");
-        return git("tag " + packageJson.version);
+        return git("tag " + packageJsonSha1);
     }).then(function () {
-        log.debug("Pushing tag " + packageJson.version + " to " + repo);
+        log.debug("Pushing tag " + packageJsonSha1 + " to " + repo);
         return git("push " + repo + " master --tags");
     });
 }
